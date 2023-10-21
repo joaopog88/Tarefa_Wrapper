ba85a02b6eefe1a379912aa6e75540214c1869d5
JoaoPaulo <jpogd@hotmail.com>
2023-10-20 21:53:16 -0300

    first commit

---
+++ src/Wrapper.java
@@ -0,0 +1,10 @@
+public class Wrapper {
+    public static void main(String args[]){
+
+        int numero = 100;
+        System.out.println(numero);
+        long numeroL = numero;
+        System.out.println(numeroL);
+
+    }
+}
